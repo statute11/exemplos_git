fc2c1b624dcc028516a282d8f85a7fdaf5baa5df
lucas <lucas@DESKTOP-E9VF47Q>
2020-08-25 20:45:40 -0300

    Commit inicial de teste

---
+++ GIT_Project/src/exemplo/Git_Project_Exemplo.java
@@ -0,0 +1,12 @@
+package exemplo;
+
+public class Git_Project_Exemplo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Olá, GIT");
+		
+	}
+
+}
