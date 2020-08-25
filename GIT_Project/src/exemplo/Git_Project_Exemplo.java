fcbc6f0c72bbd564fbc470c181d2e356e7c96717
Lucas Statute <63616548+statute11@users.noreply.github.com>
2020-08-25 20:47:10 -0300

    Update Git_Project_Exemplo.java

--- GIT_Project/src/exemplo/Git_Project_Exemplo.java
+++ GIT_Project/src/exemplo/Git_Project_Exemplo.java
@@ -5,8 +5,8 @@ public class Git_Project_Exemplo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.print("Ol·, GIT");
-		
+		System.out.print("Ol√°, GIT");
+		//alterado do github
 	}
 
 }
